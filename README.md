dacc6065c4740e27e277f64e076bfe41f83d29db
Megan Lu <f007h8x@andes8.dartmouth.edu>
2026-06-17 00:19:26 -0400

    Program design and start of guest_dir module

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # supramatch
+
+## Modules
+
+* `scraper`: scrape Sigma-Aldrich website for chemical information (name, CAS number, price, physical state, etc.)
+* `cavity_calc`: calculate the volume of the cage cavity
+* `guest_calc`: calculate the volume of the guest molecule
+* `pc_calc`: calculate the packing coefficient for the host–guest pairing
+* `hg_matcher`: evalute and select guest molecules for the host 
+
+## Common Library
+
+* `guest_dir`: functions to handle reading and writing a directory of guest molecule files
---
+++ guest_dir/guest_dir.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+"""
+File: guest_dir.py
+Author: Megan K. Lu
+Date: 06/16/2026
+Description: This module contains functions to read and write a directory of guest molecule files.
+"""
+
+from datetime import datetime
+
+def init(directory):
+    """
+    Initialize a directory for use as a guest molecule directory.
+
+    Args:
+        directory: the name of the directory.
+
+    Returns:
+        True if the directory was successfully marked as a scraper-created directory.
+        False if the directory is NULL or the marker file could not be created.
+
+    Note:
+        The directory must already exist. The function does not create it.
+    """
+
+    scraper_file = directory + "/.scraper"
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    try:
+        with open(scraper_file, "w") as file:
+            file.write(f"file created at: {current_time}\n")
+        return True
+    except OSError:
+        return False
+
+def save(directory, guest_ID, guest_info):
+    """
