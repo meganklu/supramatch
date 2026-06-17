50896071c75b9cce95ecb4ae5f2f558e45f1734e
Megan Lu <f007h8x@andes8.dartmouth.edu>
2026-06-17 18:10:21 -0400

    Reconfigured file structure

--- tests/test_cirpy_rdkit.py
+++ tests/test_cirpy_rdkit.py
@@ -2,8 +2,18 @@ import cirpy
 from rdkit import Chem
 from rdkit.Chem import AllChem
 
+# 1,3-Dibromoadamantane
 cas = '876-53-9'
 smiles = cirpy.resolve(cas, 'smiles')
+mol = Chem.MolFromSmiles(smiles) # load molecule from smiles
+mol = Chem.AddHs(mol) # add hydrogens
+AllChem.EmbedMolecule(mol, randomSeed=0xf00d) # embed to add 3D coordinates with optional random seed for reproducibility
+AllChem.MMFFOptimizeMolecule(mol) # optimize conformer with a force field
+volume = AllChem.ComputeMolVolume(mol) # calculate volume
+print("Molecular_volume= ", volume, " A3")
+
+# cis-Decalin
+smiles = "[H][C@]12CCCC[C@@]1([H])CCCC2"
 mol = Chem.MolFromSmiles(smiles) # load molecule from smiles
 mol = Chem.AddHs(mol) # add hydrogens
 AllChem.EmbedMolecule(mol, randomSeed=0xf00d) # embed to add 3D coordinates with optional random seed for reproducibility
